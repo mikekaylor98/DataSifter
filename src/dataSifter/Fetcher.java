9ddd169d64b862c4cf59595b4d0d4289a8ae78f9
mikekaylor98 <mikekaylor98@gmail.com>
2024-01-20 11:24:09 -0500

    Initial commit

---
+++ src/dataSifter/Fetcher.java
@@ -0,0 +1,5 @@
+package dataSifter;
+
+public class Fetcher {
+
+}
---
+++ src/dataSifter/Main.java
@@ -0,0 +1,7 @@
+package dataSifter;
+
+public class Main {
+public static void main(String args[]) {
+	System.out.println("You whaddup");
+}
+}
