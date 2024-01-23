d034626d43e84aa56d21d8098a9035fb4b6b77e7
mikekaylor98 <mikekaylor98@gmail.com>
2024-01-23 14:19:49 -0500

    Line reader added
    Added ability to read a CSV file, but only by a line the user wants.

--- src/dataSifter/Fetcher.java
+++ src/dataSifter/Fetcher.java
@@ -3,20 +3,41 @@ import java.io.*;
 import java.util.*;
 
 public class Fetcher {
+
 	public void fetch(String csv) {
+
 		Scanner sc;
 		try {
 			sc = new Scanner(new File(csv));
-		sc.useDelimiter("");   
-		while (sc.hasNext())
-		{  
-			System.out.print(sc.next());  
-		}   
-		sc.close();  
+			sc.useDelimiter("");   
+			while (sc.hasNext())
+			{  
+				System.out.print(sc.next());  
+			}   
+			sc.close();  
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}  
 	}	
-		
+
+	public void fetchLine(String csv, int lineNum) {
+
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader(csv));
+			List<String> lines = new ArrayList<>();
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				lines.add(line);
+			}
+
+			System.out.println(lines.get(lineNum));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
 }
--- src/dataSifter/Main.java
+++ src/dataSifter/Main.java
@@ -3,15 +3,39 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Main {
-	
-public static void main(String args[]) throws FileNotFoundException {
-	Scanner input = new Scanner(System.in);
-	System.out.println("What file do you want read?");
-	String csvName = input.next();
-	
-	Fetcher t = new Fetcher();
-	t.fetch(csvName);
-	input.close();
-}
+
+	public static void main(String args[]) throws FileNotFoundException {
+
+		Fetcher t = new Fetcher();
+		String csvName;
+		int lineNum;
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("File(1) or Line(2)?");
+		String option = input.next();
+
+		switch(option) {
+
+		case "1": 
+			System.out.println("Enter file name");
+			csvName = input.next();
+			t.fetch(csvName);
+			break;
+
+		case "2":
+			System.out.println("Enter file name");
+			csvName = input.next();
+			System.out.println("Enter line number");
+			lineNum = input.nextInt();
+			t.fetchLine(csvName, lineNum);
+			break;
+
+		default:
+			System.out.println("Invalid choice");
+
+		}
+
+		input.close();
+	}
 }
 
